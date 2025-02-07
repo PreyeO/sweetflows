1eca865811ea16b33e4a95aaa895a1e3b3baf76f
PreyeO <omusukup@yahoo.com>
2025-02-07 19:13:05 +0100

    changes

--- src/components/ServiceContainer.jsx
+++ src/components/ServiceContainer.jsx
@@ -3,33 +3,21 @@ import PropTypes from "prop-types";
 const ServiceContainer = ({
   title,
   description,
-  icon,
-  alt,
-  className,
+
   alts,
   src,
 }) => {
   return (
     <div className="flex flex-col items-center   ">
-      <div className="max-w-[350px] h-[300px] rounded-xl bg-[#333333]">
-        <img
-          src={src}
-          alt={alts}
-          className="w-full max-w-[350px] h-[300px] rounded-xl object-cover "
-        />
-      </div>
-      <div
-        className={` ${className} translate-y-[-70px] w-[300px] h-[200px] bg-[#f5f1e3] rounded-xl shadow-2xl px-4 flex flex-col gap-3 `}
-      >
-        <div className="w-[40px] h-[40px] bg-white rounded-md flex flex-col justify-center ">
-          <img
-            src={icon}
-            alt={alt}
-            className="w-[30px] h-[30px] flex flex-col"
-          />
-        </div>
-        <h4 className="text-lg font-bold">{title}</h4>
-        <p>{description}</p>
+      <img
+        src={src}
+        alt={alts}
+        className="w-full max-w-[350px] h-[300px] rounded-xl object-cover bg-[#333333] "
+      />
+
+      <div className="translate-y-[-70px] w-[300px] h-[200px] gap-4 justify-center bg-[#f5f1e3] rounded-xl shadow-2xl px-4 flex flex-col ">
+        <h4 className="text-lg font-bold  ">{title}</h4>
+        <p className=" text-base ">{description}</p>
       </div>
     </div>
   );
@@ -40,9 +28,7 @@ export default ServiceContainer;
 ServiceContainer.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
-  alt: PropTypes.string,
-  icon: PropTypes.string,
-  className: PropTypes.string,
+
   src: PropTypes.string,
   alts: PropTypes.string,
 };
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,8 @@
-import ConstructionIcon from "../assets/constructionIcon.png";
 import Construction from "../assets/construction.jpg";
 import RealEstate from "../assets/real-estate.jpg";
 import Import from "../assets/export.jpg";
 import Trading from "../assets/trading.jpg";
-import Telecommunication from "../assets/telecom.jpg";
+import Telecommunication from "../assets/telecom1.jpg";
 import ServiceContainer from "./ServiceContainer";
 import Procurement from "../assets/procurement.jpg";
 
@@ -15,58 +14,42 @@ const Services = () => {
       </h1>
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 justify-center items-center  mt-[60px] md:grid-cols-2">
         <ServiceContainer
-          alt="contrsuction icon"
           title="Construction & Building Contracts"
           description="Expert management of construction projects from start to finish."
-          icon={ConstructionIcon}
-          className=""
           alts="construction image"
           src={Construction}
         />
         <ServiceContainer
-          alt="real estate icon"
           title="Real Estate & Property Development"
           description="Innovative property development and effective estate management."
-          icon={ConstructionIcon}
-          className=""
           alts="real estate image"
           src={RealEstate}
         />
+
         <ServiceContainer
-          alt="telecommunication"
-          title="Telecommunication "
-          description="Reliable telecom solutions for seamless connectivity"
-          icon={ConstructionIcon}
-          className=""
-          alts="Telecommunication"
-          src={Telecommunication}
-        />
-        <ServiceContainer
-          alt="trading"
-          title="Trading & General Supplies"
-          description="Wide ranging quality goods offered at competitive prices"
-          icon={ConstructionIcon}
-          className=""
-          alts="Trading"
-          src={Trading}
-        />
-        <ServiceContainer
-          alt="import and export"
           title="Import & Export"
           description="Efficient global trade solutions for accessing international markets."
-          icon={ConstructionIcon}
-          className="import and export icon"
           alts="import and export"
           src={Import}
         />
         <ServiceContainer
-          alt="procurement"
+          title="Trading & General Supplies"
+          description="Wide ranging quality goods offered at competitive prices"
+          alts="Trading"
+          src={Trading}
+        />
+
+        <ServiceContainer
           title="Procurement & Sourcing Solutions"
           description="Streamlined procurement services to secure top quality goods."
-          icon={ConstructionIcon}
-          className=""
           alts="procurement"
           src={Procurement}
+        />
+        <ServiceContainer
+          title="Telecommunication Services "
+          description="Reliable telecom solutions for seamless connectivity"
+          alts="Telecommunication"
+          src={Telecommunication}
         />
       </div>
     </section>
